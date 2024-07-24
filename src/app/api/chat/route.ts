4cc8554e5a05c22382be856d964c96705e90575a
Andrei Popa <popa.andrei90@gmail.com>
2024-07-25 01:04:20 +0300

    new

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,21 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OpenAI } from "openai";
+
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+    console.log('messsages ', messages)
+
+    const openai = new OpenAI({ baseURL: process.env.API_URL, apiKey: process.env.OPENAI_API_KEY });
+
+    console.log('key :', openai.apiKey)
+    const result = await openai.chat.completions.create(
+        {
+            model: "gpt-4o-mini",
+            messages: messages,
+            stream: true,
+        }
+    );
+
+    const stream = OpenAIStream(result)
+    return new StreamingTextResponse(stream);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useChat } from "ai/react";
+
+
+export default function Robot() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({ api: process.env.API_URL });
+  return (
+    <div className="flex flex-col w-full mx-2 stretch">
+      {messages.map((message) => (
+        <div className="whitespace-pre-wrap" key={message.id}>
+          {message.role === 'user' ? 'User: ' : 'AI: '}
+          {message.content}
+        </div>
+      ))
+      }
+      <form onSubmit={handleSubmit}>
+        <input
+          autoFocus
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded text-blue-950"
+          type="text"
+          value={input}
+          placeholder="Says something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
