0353c045696e968af9f9dba362b8164b846dc841
Andrei Popa <popa.andrei90@gmail.com>
2024-07-24 23:23:45 +0300

    draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useChat } from "ai/react";
 
 export default function Robot() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({ api: "api/chat" });
-
+  console.log(process.env.OPENAI_API_KEY);
   return (
     <div className="flex flex-col w-full mx-2 stretch">
       {messages.map((message) => (
@@ -17,6 +17,7 @@ export default function Robot() {
       }
       <form onSubmit={handleSubmit}>
         <input
+          autoFocus
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded text-blue-950"
           type="text"
           value={input}
