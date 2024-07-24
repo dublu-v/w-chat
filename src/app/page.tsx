960d975cddc84c33d79c2d9cddef45f7acc9addc
Andrei Popa <popa.andrei90@gmail.com>
2024-07-25 01:38:37 +0300

    updated env vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useChat } from "ai/react";
 
 
 export default function Robot() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({ api: process.env.API_URL });
+  const { messages, input, handleInputChange, handleSubmit } = useChat({ api: process.env.NEXT_PUBLIC_API_URL });
   console.log(process.env.API_URL);
   return (
     <div className="flex flex-col w-full mx-2 stretch">
