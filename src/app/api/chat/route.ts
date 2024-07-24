92139166ab56b0d81de0d38bfe0c36dedf05adca
Andrei Popa <popa.andrei90@gmail.com>
2024-07-25 02:46:30 +0300

    try with @ai-sdk

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,21 +1,27 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { OpenAIStream, StreamingTextResponse, streamText } from "ai";
 import { OpenAI } from "openai";
+import { openai } from '@ai-sdk/openai';
 
 export async function POST(req: Request) {
     const { messages } = await req.json();
     console.log('messsages ', messages)
 
-    const openai = new OpenAI();
+    //const openai = new OpenAI({ baseURL: 'https://api.openai.com/v1' });
 
-    console.log('key :', openai.apiKey)
-    const result = await openai.chat.completions.create(
-        {
-            model: "gpt-4o-mini",
-            messages: messages,
-            stream: true,
-        }
-    );
+    const result = await streamText({
+        model: openai('gpt-4o-mini'),
+        messages,
+    })
+    return result.toAIStreamResponse();
+    // console.log('key :', openai.apiKey)
+    // const result = await openai.chat.completions.create(
+    //     {
+    //         model: "gpt-4o-mini",
+    //         messages: messages,
+    //         stream: true,
+    //     }
+    // );
 
-    const stream = OpenAIStream(result)
-    return new StreamingTextResponse(stream);
+    // const stream = OpenAIStream(result)
+    // return new StreamingTextResponse(stream);
 }
