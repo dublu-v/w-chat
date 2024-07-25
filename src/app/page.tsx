ad51f973a397893b88ed146fa17832532130d70b
Andrei Popa <popa.andrei90@gmail.com>
2024-07-25 23:31:43 +0300

    ssr option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useCompletion } from 'ai/react';
+import { readStreamableValue } from 'ai/rsc';
 import { useState } from 'react';
+import { generate } from './actions';
 
 
 export default function Robot() {
@@ -8,14 +10,34 @@ export default function Robot() {
 
   const [input, setInput] = useState('');
 
-  return (<>
-    <input className="text-blue-950" value={input} onChange={(e) => setInput(e.target.value)} />
-    <div
-      onClick={async () => {
-        await complete(input);
-      }}
-    >Click to ask
-    </div><div>{completion}</div>
-  </>
+  const [generation, setGeneration] = useState<string>('');
+
+  return (
+    <><div>
+      <button
+        onClick={async () => {
+          const { output } = await generate('Why is the sky blue?');
+
+          for await (const delta of readStreamableValue(output)) {
+            setGeneration(currentGeneration => `${currentGeneration}${delta}`);
+          }
+        }}
+      >
+        Ask
+      </button>
+
+      <div>{generation}</div>
+    </div>
+      <br />
+
+
+      <input className="text-blue-950" value={input} onChange={(e) => setInput(e.target.value)} />
+      <div
+        onClick={async () => {
+          await complete(input);
+        }}
+      >Click to ask
+      </div><div>{completion}</div>
+    </>
   );
 }
